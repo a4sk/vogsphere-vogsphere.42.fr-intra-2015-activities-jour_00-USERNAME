f97d20531a36175ecc145572b6f0eb19f365f7bd
a4sk <re.play@yahoo.com>
2016-01-23 01:22:33 +0200

    01

---
+++ Libft/ft_strtrim.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dgenes <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/12/17 17:02:17 by dgenes            #+#    #+#             */
+/*   Updated: 2016/01/22 19:35:05 by dgenes           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdlib.h>
+
+char	*ft_strtrim(char const *s)
+{
+	size_t		i;
+	size_t		i2;
+	size_t		max;
+	char		*new;
+
+	i = 0;
+	i2 = 0;
+	if (ft_strlen(s) == 0)
+		return ((char *)s);
+	max = ft_strlen(s) - 1;
+	new = (char *)malloc((ft_strlen(s) + 1) * sizeof(char));
+	if (new)
+	{
+		while (s[i2] == ' ' || s[i2] == '\n' || s[i2] == '\t')
+			i2++;
+		if (i2 < max)
+		while (s[max] == ' ' || s[max] == '\n' || s[max] == '\t')
+				max--;
+		while (i2 <= max)
+			new[i++] = s[i2++];	
+		new[i] = '\0';
+		return (new);
+	}
+	return (NULL);
+}
